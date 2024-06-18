cccf8161782ee77e04d32ff94f96a3bb15f55981
thisisaarush <tanwaraarush007@gmail.com>
2024-06-19 03:29:38 +0530

    Build the new snip page with form validation with zod

--- src/app/snips/create/page.tsx
+++ src/app/snips/create/page.tsx
@@ -1,25 +1,99 @@
 "use client"
 
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import { useState } from "react"
+
+const fileSchema = z.object({
+  name: z.string(),
+  size: z.number(),
+  type: z.string(),
+  lastModified: z.number()
+})
+
+const snipFormSchema = z.object({
+  description: z.string().min(5).max(200),
+  files: z.array(fileSchema).max(5).nonempty()
+})
 
 const CreateSnipPage = () => {
+  const [fileNames, setFileNames] = useState<string[]>([])
+
+  const form = useForm<z.infer<typeof snipFormSchema>>({
+    resolver: zodResolver(snipFormSchema)
+  })
+
+  const onSubmit = (data: z.infer<typeof snipFormSchema>) => {
+    console.log("Form data:", data)
+  }
+
   return (
     <div className="m-auto flex max-w-4xl flex-col gap-10 p-6">
       <h1 className="text-xl font-medium md:text-2xl">Create a new Snip</h1>
-      <div className="flex flex-col gap-4">
-        <Label htmlFor="snip-description">Description</Label>
-        <Textarea
-          id="snip-description"
-          placeholder="Snip description..."
-          className="max-h-[400px] min-h-32"
-        />
-      </div>
-
-      <Button variant="default" size="lg">
-        Create Snip
-      </Button>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Snip Description..."
+                    {...field}
+                    className="max-h-[400px] min-h-32"
+                  />
+                </FormControl>
+                <FormDescription>Briefly describe what this snip is doing.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="files"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Code Files</FormLabel>
+                <FormControl>
+                  <Input
+                    type="file"
+                    multiple
+                    accept=".js, .jsx, .ts, .tsx, .java, .py, .cpp, .c, .h, .html, .css, .scss, .php, .rb, .go, .rs, .swift, .kt, .json, .xml, .yaml, .md, .txt, !image/*, !.pdf"
+                    onChange={(event) => {
+                      const files = event.target.files ? Array.from(event.target.files) : []
+                      setFileNames(files.map((file) => file.name))
+                      field.onChange(files)
+                    }}
+                  />
+                </FormControl>
+                <FormDescription>Upload code files for the snip.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <ul className="mt-8 flex flex-col gap-2 text-sm">
+            {fileNames?.map((name, index) => <li key={index}>{name}</li>)}
+          </ul>
+          <Button type="submit" className="w-full">
+            Create Snip
+          </Button>
+        </form>
+      </Form>
     </div>
   )
 }
