160c347308bea1498bb3c0e6922f5f2eed652a9d
thisisaarush <tanwaraarush007@gmail.com>
2024-06-23 03:29:20 +0530

    identifyLanguageByExtension funtion added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,43 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useUser } from "@clerk/nextjs"
+import CodeBlock from "@/components/custom/code-block"
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
+import SnipCards from "@/components/custom/snip-cards"
+
+// function to identify the programming language by the file extension
+function identifyLanguageByExtension(fileName: string): string {
+  const extensionToLanguageMap: { [key: string]: string } = {
+    ".js": "JavaScript",
+    ".jsx": "JavaScript",
+    ".ts": "TypeScript",
+    ".tsx": "TypeScript",
+    ".py": "Python",
+    ".java": "Java",
+    ".cs": "C#",
+    ".cpp": "C++",
+    ".rb": "Ruby",
+    ".php": "PHP",
+    ".html": "HTML",
+    ".css": "CSS",
+    ".scss": "SASS/SCSS",
+    ".md": "Markdown",
+    ".json": "JSON",
+    ".xml": "XML"
+  }
+
+  const extension = fileName.slice(fileName.lastIndexOf(".")).toLowerCase()
+  const language = extensionToLanguageMap[extension]
+
+  return language?.toLowerCase() || ""
+}
 
 const DashboardPage = () => {
   const { user, isLoaded, isSignedIn } = useUser()
   const email = user?.primaryEmailAddress?.emailAddress || ""
+
+  const [snips, setSnips] = useState([])
 
   useEffect(() => {
     const fetchSnips = async () => {
@@ -24,6 +56,7 @@ const DashboardPage = () => {
 
         const data = await res.json()
         console.log("Snips:", data.snips)
+        setSnips(data.snips)
       } catch (error) {
         console.error("An error occurred while getting snips:", error)
       }
@@ -35,7 +68,7 @@ const DashboardPage = () => {
   }, [email, isLoaded, isSignedIn])
 
   return (
-    <div>
+    <div className="m-auto min-h-screen max-w-5xl py-4">
       <h1>Dashboard</h1>
     </div>
   )
